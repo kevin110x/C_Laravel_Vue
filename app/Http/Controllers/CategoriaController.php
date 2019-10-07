3b88453d422260124c5f6171b498bf42c18e65d5
Kevin López <kevinlopez.master2000@gmail.com>
2019-10-07 17:57:54 -0500

    First commit

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Categoria;
+
+class CategoriaController extends Controller
+{
+
+    public function index()
+    {
+        $categorias = Categoria::all();
+        return $categorias;
+    }
+
+    public function store(Request $request)
+    {
+        $categoria = new Categoria();
+        $categoria->nombre = $request->nombre;
+        $categoria->descripcion = $request->descripcion;
+        $categoria->condicion = '1';
+        $categoria->save();
+    }
+
+    public function update(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->nombre = $request->nombre;
+        $categoria->descripcion = $request->descripcion;
+        $categoria->condicion = '1';
+        $categoria->save();
+    }
+
+    public function desactivar(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->condicion = '0';
+        $categoria->save();
+    }
+
+    public function activar(Request $request)
+    {
+        $categoria = Categoria::findOrFail($request->id);
+        $categoria->condicion = '1';
+        $categoria->save();
+    }
+
+}
---
+++ resources/views/content/content.blade.php
@@ -0,0 +1,57 @@
+@extends('principal')
+@section('content')
+
+
+    <template v-if="menu==0">
+       
+    </template>
+
+    <template v-if="menu==1">
+        <Categoria></Categoria>
+    </template>
+
+    <template v-if="menu==2">
+        <h1>Estado del menu... 2</h1>
+    </template>
+
+    <template v-if="menu==3">
+        <h1>Estado del menu... 3</h1>
+    </template>
+
+    <template v-if="menu==4">
+        <h1>Estado del menu... 4</h1>
+    </template>
+
+    <template v-if="menu==5">
+        <h1>Estado del menu... 5</h1>
+    </template>
+
+    <template v-if="menu==6">
+        <h1>Estado del menu... 6</h1>
+    </template>
+
+    <template v-if="menu==7">
+        <h1>Estado del menu... 7</h1>
+    </template>
+
+    <template v-if="menu==8">
+        <h1>Estado del menu... 8</h1>
+    </template>
+
+    <template v-if="menu==9">
+        <h1>Estado del menu... 9</h1>
+    </template>
+
+    <template v-if="menu==10">
+        <h1>Estado del menu... 10</h1>
+    </template>
+
+    <template v-if="menu==11">
+        <h1>Estado del menu... 11</h1>
+    </template>
+
+    <template v-if="menu==12">
+        <h1>Estado del menu... 12</h1>
+    </template>
+
+@endsection
