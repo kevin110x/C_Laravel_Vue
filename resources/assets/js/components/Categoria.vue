c276ef93307ec7dcb7c71542c2ca584e269bf701
Kevin López <kevinlopez.master2000@gmail.com>
2019-11-13 07:46:41 -0500

    Proceso cap 26

--- resources/assets/js/components/Categoria.vue
+++ resources/assets/js/components/Categoria.vue
@@ -11,7 +11,7 @@
         <div class="card">
             <div class="card-header">
                 <i class="fa fa-align-justify"></i> Categorías
-                <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalNuevo">
+                <button type="button" class="btn btn-secondary" @click="abrirModal('categoria','registrar')">
                     <i class="icon-plus"></i>&nbsp;Nuevo
                 </button>
             </div>
@@ -50,10 +50,10 @@
                             <td><h5 v-text="categoria.nombre"></h5></td>
                             <td><h5 v-text="categoria.descripcion"></h5></td>
                             <td>
-                                <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#modalNuevo">
+                                <button type="button" class="btn btn-info btn-sm" @click="abrirModal('categoria','actualizar',categoria)">
                                     <i class="icon-pencil"></i>
                                 </button> &nbsp;
-                                <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modalEliminar">
+                                <button type="button" class="btn btn-danger btn-sm">
                                     <i class="icon-trash"></i>
                                 </button>
                             </td>
@@ -87,12 +87,12 @@
         <!-- Fin ejemplo de tabla Listado -->
     </div>
     <!--Inicio del modal agregar/actualizar-->
-    <div class="modal fade" id="modalNuevo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+    <div class="modal fade" tabindex="-1" :class="{'mostrar' : modal}" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
         <div class="modal-dialog modal-primary modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">Agregar categoría</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <h4 class="modal-title" v-text="tituloModal"></h4>
+                    <button type="button" class="close" @click="cerrarModal()" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
@@ -101,21 +101,22 @@
                         <div class="form-group row">
                             <label class="col-md-3 form-control-label" for="text-input">Nombre</label>
                             <div class="col-md-9">
-                                <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre de categoría">
+                                <input type="text" v-model="nombre" class="form-control" placeholder="Nombre de categoría">
                                 <span class="help-block">(*) Ingrese el nombre de la categoría</span>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-md-3 form-control-label" for="email-input">Descripción</label>
                             <div class="col-md-9">
-                                <input type="email" id="descripcion" name="descripcion" class="form-control" placeholder="Enter Email">
+                                <input type="email" v-model="descripcion" class="form-control" placeholder="Enter Email">
                             </div>
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                    <button type="button" class="btn btn-primary">Guardar</button>
+                    <button type="button" class="btn btn-secondary" @click="cerrarModal()">Cerrar</button>
+                    <button type="button" class="btn btn-primary" v-if="tipoAccion == 1">Guardar</button>
+                    <button type="button" class="btn btn-primary" v-if="tipoAccion == 2">Actualizar</button>
                 </div>
             </div>
             <!-- /.modal-content -->
@@ -155,11 +156,14 @@
             return{
                 nombre: "",
                 descripcion: "",
-                arrayCategoria: []
+                arrayCategoria: [],
+                modal: 0,
+                tituloModal: '',
+                tipoAccion: 0
             }
         },
         methods: {
-            listarCategoria(){
+            listarCategoria() {
                 let me = this;
                 axios.get('/categoria').then(function (response) {
                         me.arrayCategoria = response.data;
@@ -171,6 +175,36 @@
                     .finally(function () {
                         // always executed
                     });
+            },
+            registrarCategoria() {
+
+            },
+
+            cerrarModal() {
+                this.modal = 0;
+                this.tituloModal = '';
+                this.nombre = '';
+                this.descripcion = '';
+            },
+
+            abrirModal(modelo, accion, data = []) {
+                switch (modelo) {
+                    case "categoria": {
+                        switch (accion) {
+                            case 'registrar': {
+                                this.modal = 1;
+                                this.tituloModal = 'Registrar Categoría';
+                                this.nombre = '';
+                                this.descripcion = '';
+                                this.tipoAccion = 1;
+                                break
+                            }
+                            case 'actualizar': {
+
+                            }
+                        }
+                    }
+                }
             }
         },
         mounted() {
@@ -178,3 +212,18 @@
         }
     }
 </script>
+
+<style>
+    .modal-content {
+        width: 100% !important;
+        position: absolute !important;
+    }
+
+    .mostrar {
+        display: list-item !important;
+        opacity: 1 !important;
+        position: absolute !important;
+        background-color: #3c29297a !important;
+    }
+
+</style>
